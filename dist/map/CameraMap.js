9d696e5cd2ef1c99523aa2904aaefb6426855163
Marco Q <marcoq@gmail.com>
2021-09-29 09:50:40 +0200

    export dist and types

--- dist/map/CameraMap.js
+++ dist/map/CameraMap.js
@@ -2,7 +2,11 @@ import turfBbox from '@turf/bbox';
 import turfBboxPolygon from '@turf/bbox-polygon';
 import turfCircle from '@turf/circle';
 import turfTransformRotate from '@turf/transform-rotate';
+import turfBearing from '@turf/bearing';
+import turfDistance from '@turf/distance';
+import { CAMERA_FOV, Projection } from 'threebox';
 import { Map as MapGL, LngLat } from 'mapbox-gl';
+import { clamp, toDegrees, wrap } from '../utils/math';
 export class CameraMap {
     constructor(containerElement, mapToken, mapStyle) {
         this._mapGL = new MapGL({
@@ -32,9 +36,13 @@ export class CameraMap {
         });
     }
     setStyle(styleUrl) {
-        if (!styleUrl)
+        this._mapStyle = styleUrl;
+        if (!this._mapStyle)
             return;
-        this._mapGL.setStyle(styleUrl);
+        this._mapGL.setStyle(this._mapStyle);
+    }
+    getStyle() {
+        return this._mapStyle;
     }
     setInteractive(interactive) {
         const action = interactive ? 'enable' : 'disable';
@@ -65,19 +73,20 @@ export class CameraMap {
         });
     }
     getCameraFromFraming(framing) {
-        if (framing.center && framing.radius) {
-            const bbox = rectFromCenterRadiusAndAngle(framing.center, framing.radius, framing.bearing);
-            framing.bounds = [bbox[0], bbox[2]];
+        const fr = { ...framing };
+        if (fr.center && fr.radius) {
+            const bbox = rectFromCenterRadiusAndAngle(fr.center, fr.radius, fr.bearing);
+            fr.bounds = [bbox[0], bbox[2]];
         }
-        else if (!framing.bounds || !framing.bounds.length) {
+        else if (!fr.bounds || !fr.bounds.length) {
             throw new Error('Framing data needs either bounds or center and radius');
         }
-        const cameraOptions = this._mapGL._cameraForBoxAndBearing(framing.bounds[0], framing.bounds[1], framing.bearing, {
+        const cameraOptions = this._mapGL._cameraForBoxAndBearing(fr.bounds[0], fr.bounds[1], fr.bearing, {
             padding: {
-                top: resolvePercent(framing.padding?.top, this._mapGL.transform.height),
-                bottom: resolvePercent(framing.padding?.bottom, this._mapGL.transform.height),
-                left: resolvePercent(framing.padding?.left, this._mapGL.transform.width),
-                right: resolvePercent(framing.padding?.right, this._mapGL.transform.width),
+                top: resolvePercent(fr.padding?.top, this._mapGL.transform.height),
+                bottom: resolvePercent(fr.padding?.bottom, this._mapGL.transform.height),
+                left: resolvePercent(fr.padding?.left, this._mapGL.transform.width),
+                right: resolvePercent(fr.padding?.right, this._mapGL.transform.width),
             },
         });
         if (!cameraOptions)
@@ -88,6 +97,37 @@ export class CameraMap {
             pitch: framing.pitch,
             center: LngLat.convert(cameraOptions.center),
         };
+    }
+    zoomCenterFromCoords(coords) {
+        const center = LngLat.convert([coords[0], coords[1]]);
+        const zoom = this.altitudeToZoom(coords[1], coords[2]);
+        return { center, zoom };
+    }
+    cameraFromTarget(eye, target) {
+        const bearing = bearingFromCoords(eye, target);
+        const pitch = pitchFromCoords(eye, target);
+        return { ...this.zoomCenterFromCoords(eye), pitch, bearing };
+    }
+    coordsFromCamera(pos) {
+        if (!this.mapGL)
+            throw new Error('MapGL not initialized yet.');
+        const zoom = clamp(pos.zoom, this.mapGL.getMinZoom(), this.mapGL.getMaxZoom());
+        const height = this.zoomToAltitude(pos.center.lat, zoom);
+        return [pos.center.lng, pos.center.lat, height];
+    }
+    cameraToCenterDistance() {
+        const halfFov = CAMERA_FOV / 2;
+        return (0.5 / Math.tan(halfFov)) * this.mapGL.transform.height;
+    }
+    zoomToAltitude(lat, zoom) {
+        const scale = Projection.zoomToScale(zoom) * (Projection.TILE_SIZE / Projection.WORLD_SIZE);
+        const pxPerMeter = Projection.projectedUnitsPerMeter(lat) * scale;
+        return this.cameraToCenterDistance() / pxPerMeter;
+    }
+    altitudeToZoom(lat, height) {
+        const pxPerMeter = this.cameraToCenterDistance() / height;
+        const scale = pxPerMeter / Projection.projectedUnitsPerMeter(lat) / (Projection.TILE_SIZE / Projection.WORLD_SIZE);
+        return Projection.scaleToZoom(scale);
     }
 }
 function resolvePercent(value, full) {
@@ -109,5 +149,12 @@ function rectFromCenterRadiusAndAngle(center, radius, angle) {
     const rotatedPolygon = turfTransformRotate(bboxPolygon, angle);
     const vertices = rotatedPolygon.geometry.coordinates[0].slice(0, 4);
     return vertices;
+}
+export const bearingFromCoords = turfBearing;
+export function pitchFromCoords(eye, target) {
+    const altitude = eye[2];
+    const groundDistance = turfDistance(eye, target);
+    const rad = Math.atan2(altitude - target[2], groundDistance);
+    return wrap(90 - toDegrees(rad), 0, 360);
 }
 //# sourceMappingURL=CameraMap.js.map
--- types/map/CameraMap.d.ts
+++ types/map/CameraMap.d.ts
@@ -1,14 +1,28 @@
-import { FlyToOptions } from 'mapbox-gl';
-import { CameraFramingData, CameraPosition } from './Camera';
+import turfBearing from '@turf/bearing';
+import { FlyToOptions, LngLat } from 'mapbox-gl';
+import { CameraFramingData, CameraPosition, coord3 } from './Camera';
 import { ExtendedMapGL } from './ExtendedMapGl';
 export declare class CameraMap {
     private _mapGL;
+    private _mapStyle;
     get mapGL(): ExtendedMapGL;
     constructor(containerElement: HTMLElement, mapToken: string, mapStyle: string);
     ready(): Promise<void>;
     setStyle(styleUrl?: string): void;
+    getStyle(): string | undefined;
     setInteractive(interactive: boolean): void;
     update3D(): void;
     flyTo(options: FlyToOptions): Promise<void>;
     getCameraFromFraming(framing: CameraFramingData): CameraPosition;
+    zoomCenterFromCoords(coords: coord3): {
+        center: LngLat;
+        zoom: number;
+    };
+    cameraFromTarget(eye: coord3, target: coord3): CameraPosition;
+    coordsFromCamera(pos: CameraPosition): coord3;
+    cameraToCenterDistance(): number;
+    zoomToAltitude(lat: number, zoom: number): number;
+    altitudeToZoom(lat: number, height: number): number;
 }
+export declare const bearingFromCoords: typeof turfBearing;
+export declare function pitchFromCoords(eye: coord3, target: coord3): number;
